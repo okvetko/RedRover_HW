dcdeaf4edc6a1b95b918a777a25cb41f4d87ddbe
olga-kvetko-idf <olga.kvetko@idfinance.com>
2024-08-30 01:06:39 +0300

    HW - 3.1.1

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 6;
+        if(a == b ){
+            System.out.println("a==b");
+        } else if (a < b) {
+            System.out.println("a<b");
+        } else if (a > b) {
+            System.out.println("a> b");
+        }
+
+
+    }
+}
