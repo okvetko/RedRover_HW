0c0defd576e27d6551ce1cf36efd19679fed0f76
olga-kvetko-idf <olga.kvetko@idfinance.com>
2024-08-30 23:00:13 +0300

    HW_3_2_1

---
+++ src/HW_3/HW_3_2_1.java
@@ -0,0 +1,14 @@
+package HW_3;
+
+public class HW_3_2_1 {
+    public static void main(String[] args) {
+        /*
+        Необходимо вывести числа от 0 до 15.
+
+         */
+        for (int i = 0; i < 16; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
