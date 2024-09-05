27fe1ba6d3d1a99bc44dde4a1905b4c2ae2d1141
olga-kvetko-idf <olga.kvetko@idfinance.com>
2024-09-05 22:44:55 +0300

    HW_5

--- src/HW_5/HW_5_6.java
+++ src/HW_5/HW_5_6.java
@@ -1,16 +1,18 @@
 package HW_5;
 
-public class HW_5_5 {
+public class HW_5_6 {
     public static void main(String[] args) {
         /*Дан массив:
 int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
-необходимо вывести сумму элементов массива.
+необходимо вывести максимальное значение массива.
 */
         int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
-        int result = 0;
+        int result = Integer.MIN_VALUE;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                result += array[i][j];
+                if(result < array[i][j]){
+                    result = array[i][j];
+                }
             }
 
         }
