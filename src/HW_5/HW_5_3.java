aaf749fa13e31ba4bba87baf8d6a2e5ef5d2c946
olga-kvetko-idf <olga.kvetko@idfinance.com>
2024-09-05 22:26:53 +0300

    HW_5

---
+++ src/HW_5/HW_5_3.java
@@ -0,0 +1,18 @@
+package HW_5;
+
+public class HW_5_3 {
+    public static void main(String[] args) {
+        /*Дан массив:
+int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+необходимо вывести минимальное значение массива.
+*/
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if(result > array[i]){
+                result=array[i];
+            }
+        }
+        System.out.println(result);
+    }
+}
