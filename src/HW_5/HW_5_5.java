eaafa49456c2ab2b500438b4a614cdc8d71589f9
olga-kvetko-idf <olga.kvetko@idfinance.com>
2024-09-05 22:39:59 +0300

    HW_5

---
+++ src/HW_5/HW_5_5.java
@@ -0,0 +1,19 @@
+package HW_5;
+
+public class HW_5_5 {
+    public static void main(String[] args) {
+        /*Дан массив:
+int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+необходимо вывести сумму элементов массива.
+*/
+        int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+        int result = 0;
+        for (int i = 0; i < array[i].length; i++) {
+            for (int j = 0; j < array[j].length; j++) {
+                result += array[i][j];
+            }
+
+        }
+        System.out.println(result);
+    }
+}
