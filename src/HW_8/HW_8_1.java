da91241b8129342cce648ac9f71707c1da2cd369
olga-kvetko-idf <olga.kvetko@idfinance.com>
2024-10-07 00:23:33 +0300

    HW_8

--- src/HW_8/HW_8_1.java
+++ src/HW_8/HW_8_1.java
@@ -1,16 +1,24 @@
-//package HW_8;
-//
-//public class HW_8_1 {
-//    public static void main(String[] args) {
-//        System.out.println(numberToText(444));
-//    }
-//    public static String numberToText (int number){
-//        String[] words = {"ноль", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
-//        char[] digitChars = String.valueOf(number).toCharArray(); //char[]
-//        for (char digitChar: digitChars) {
-//            String word;
-//
-//        }
-//
-//    }
-//}
+package HW_8;
+
+public class HW_8_1 {
+    public static void main(String[] args) {
+        System.out.println(numberToText(444));
+    }
+    public static String numberToText (int number){
+
+        String[] words = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+
+        char[] digitChars = String.valueOf(number).toCharArray(); //char[]
+
+        String result = "";
+        String separator = "";
+         for (char digitChar: digitChars) {
+            int digitInt = Integer.parseInt(String.valueOf(digitChar));
+            String word = words[digitInt];
+            result += separator + word;
+            separator = " ";
+
+        }
+        return result;
+    }
+}
